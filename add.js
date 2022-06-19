c3e57f63b25ad993480bf984b748f2f9976fe320
hamza javed <hamzajav12@gmail.com>
2022-06-19 14:57:57 +0500

    update addition

--- add.js
+++ add.js
@@ -1,5 +1,16 @@
+
 let a ,b ;
 a = 10;
 b= 5 ;
 let c = a+b;
 console.log(` the sum is ${c}`);
+
+// sweap 2 number 
+let d =1;
+let e =2;
+let f ;
+
+f=d;
+d=e;
+e=f;
+console.log(` the result is    d: ${d} and  e:${e}`);
